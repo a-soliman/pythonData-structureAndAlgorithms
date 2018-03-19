0003dee560d9fc917e34720eff44f55165aa83f7
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-19 11:08:52 -0700

    created the DynamicArrray class inheriteing the object

--- sec-12-arrays/dynamicArray.py
+++ sec-12-arrays/dynamicArray.py
@@ -1,6 +1,13 @@
 import ctypes
 
+class DynamicArray(object):
+      
+      def __init__(self):
+            self.len          = 0
+            self.capacity     = 1
+            self.arr          = self.make_array(self.capacity)
 
+      
 
 
 
