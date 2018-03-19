d2b91a95d71b98aece6faee6413bb11cfcc762fd
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-19 12:06:56 -0700

    is_anagram problem

---
+++ sec-12-arrays/problems.py
@@ -0,0 +1,13 @@
+# Anagram Check
+
+'''
+Given two strings, check to see if they are anagrams. An anagram is when the two strings can 
+be written using the exact same letters (so you just rearrange the letters to get the diffrent phrase or word).
+
+FOR EXAMPLE:
+      'public relations' is an anagram of 'crap built on lies.'
+      'client eastwood' is an anagram of 'old west action'
+
+- ignore spaces and capitalizations 
+'''
+
