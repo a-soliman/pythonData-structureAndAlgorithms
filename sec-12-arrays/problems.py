8be5bee00d84e6015d4b95e0e5d5c810c72e461d
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-19 12:07:25 -0700

    solved is_anagram problem using dictionary

--- sec-12-arrays/problems.py
+++ sec-12-arrays/problems.py
@@ -11,3 +11,31 @@ FOR EXAMPLE:
 - ignore spaces and capitalizations 
 '''
 
+def is_anagram(str1, str2):
+      str1 = str1.replace(' ', '').lower()
+      str2 = str2.replace(' ', '').lower()
+
+      letters_count = {}
+
+      for letter in str1:
+            if letter in letters_count:
+                  letters_count[letter] += 1
+            else:
+                  letters_count[letter] = 1 
+
+      for letter in str2:
+            if letter in letters_count and letters_count[letter] > 0:
+                  letters_count[letter] -= 1
+            else:
+                  return False
+      
+      for letter in letters_count:
+            if letters_count[letter] != 0:
+                  return False
+      
+      return True
+
+s1 = 'clint eastwood'
+s2 = 'old west action'
+
+print('is_anagram:\n {} || {}. = '.format(s1, s2), is_anagram(s1, s2))
