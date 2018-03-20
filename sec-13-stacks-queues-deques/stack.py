ee723e4b93ea3a56c50f2387e5faac5fe99f9050
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-20 08:57:33 -0700

    coded a stack classs with push pop, peek, isEmpty, and size methods

--- sec-13-stacks-queues-deques/stack.py
+++ sec-13-stacks-queues-deques/stack.py
@@ -1,1 +1,31 @@
-# implement a stck
+# implement a stack
+
+class Stack(object):
+
+      def __init__(self):
+            self.items = []
+
+      def push(self, item):
+            self.items.append(item)
+
+      def pop(self):
+            item = self.items.pop()
+            return item
+
+      def peek(self):
+            return self.items[len(self.items)-1]
+      
+      def isEmpty(self):
+            return len(self.items) == 0
+      
+      def size(self):
+            return len(self.items)
+
+
+stack = Stack()
+stack.push('Hello')
+stack.push('Hey')
+# print(stack.pop())
+print(stack.peek())
+print('* isEmpty(): ', stack.isEmpty())
+print('* size(): ', stack.size())
