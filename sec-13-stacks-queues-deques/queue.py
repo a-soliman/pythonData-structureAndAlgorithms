5d363bac47a2660bf6c88175eebc70b7eb051665
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-20 09:10:44 -0700

    coded a queue class

--- sec-13-stacks-queues-deques/queue.py
+++ sec-13-stacks-queues-deques/queue.py
@@ -1,1 +1,27 @@
 # implement a Queue
+class Queue(object):
+      def __init__(self):
+            self.items = []
+
+      def enqueue(self, item):
+            self.items.append(item)
+
+      def dequeue(self):
+            item = self.items.pop(0)
+            return item
+
+      def isEmpty(self):
+            return len(self.items) == 0
+
+      def size(self):
+            return len(self.items)
+
+
+q = Queue()
+q.enqueue('20')
+q.enqueue('23')
+print(q.items)
+print(q.dequeue())
+print('size(): ', q.size())
+print('isEmpty(): ', q.isEmpty())
+
