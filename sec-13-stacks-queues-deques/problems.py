f04358aa72781636b9d186c595752f1291f5aa21
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-20 16:34:15 -0700

    halfwaythrough

---
+++ sec-13-stacks-queues-deques/problems.py
@@ -0,0 +1,24 @@
+#=======================================================================================
+
+# Balanced Prentheses
+
+'''
+Given a string of opening and closing parentheses, check whether it's balanced.
+Assume that the string doesn't contain words or numbers, and you can assume that the string has no spaces
+
+
+EX:
+      '([])'
+      output True
+EX:
+      '([)]'
+      output: False
+'''
+
+def balance_check(string):
+      return string
+      
+      
+
+
+print('13- balance_check: ', balance_check('([])'))
