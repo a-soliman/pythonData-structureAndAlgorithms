e3a1f8cdea5dc24faf25c7d1cd37757999971415
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-21 07:26:20 -0700

    solved the balance_check problem

--- sec-13-stacks-queues-deques/problems.py
+++ sec-13-stacks-queues-deques/problems.py
@@ -16,9 +16,23 @@ EX:
 '''
 
 def balance_check(string):
-      return string
-      
+      if len(string) == 0:
+            return 
+      openining = ('(', '{', '[')
+      matches = set([ ('(', ')'), ('[', ']'), ('{', '}') ])
+      stack =[]
+
+      for char in string:
+            if char in openining:
+                  stack.append(char)
+            else:
+                  for o,c in matches:
+                        if c == char:
+                              if o != stack.pop():
+                                    return False
+      return True
+                                    
       
 
 
-print('13- balance_check: ', balance_check('([])'))
+print('13- balance_check: ', balance_check('([[]])'))
