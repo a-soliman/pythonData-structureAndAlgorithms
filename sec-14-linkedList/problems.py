7f66054e8ea8939c7fa97f119dd7e94b8f454b9e
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-22 15:36:17 -0700

    coded an additional soulution for checking cycle list

--- sec-14-linkedList/problems.py
+++ sec-14-linkedList/problems.py
@@ -39,6 +39,20 @@ c = Node(3)
 
 a.next = b
 b.next = c
-c.next = a
+# c.next = a
 
-print(check_cycle(a))
+print('17- cycleList: ', check_cycle(a))
+
+def check_cycle_2(node):
+      marker1 = node
+      marker2 = node
+
+      while marker2.next != None and marker2.next.next != None:
+            marker1 = marker1.next
+            marker2 = marker2.next.next
+
+            if marker1 == marker2:
+                  return True
+      return False
+
+print('18- cycleList 2 : ', check_cycle_2(a))
