7468a19b82c2571089b3e68df46095748996fd9a
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-25 08:52:32 -0700

    working on recursion

---
+++ sec-15-recursion/hw.py
@@ -0,0 +1,21 @@
+'''
+
+'''
+
+
+#=======================================================================================
+
+# recursive Sum
+
+'''
+Write a recursive function that returns the sum from 0 up to n
+
+'''
+
+def sum_down( n ):
+      if n == 1:
+            return 1
+      else:
+            return n + sum_down(n-1)
+
+print('23- sum_down: ', sum_down(5))
---
+++ sec-15-recursion/recursion.py
@@ -0,0 +1,22 @@
+'''
+n factorial
+'''
+
+def factorial( n ):
+      # base case
+      if n == 0:
+            return 1
+
+      else:
+            return n * factorial(n-1)
+
+print(factorial(5))
+
+
+def sum_down( n ):
+      if n == 1:
+            return 1
+      else:
+            return n + sum_down(n-1)
+
+print(sum_down(5))
