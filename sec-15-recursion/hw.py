0f243b9640ad131191dcdb991471e14c190e8f5e
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-25 09:33:47 -0700

    solved the fibonacci using a helper function

--- sec-15-recursion/hw.py
+++ sec-15-recursion/hw.py
@@ -31,3 +31,25 @@ print('23- sum_down: ', sum_down(5))
 
 '''
 
+def nth_fibonacci( n ):
+      fibonacci = [0, 1]
+
+      # helper function
+      def search(x, limit):
+            #base-case
+            if x == limit:
+                  return
+            
+            sum  = fibonacci[x-1] + fibonacci[x-2]
+            fibonacci.append(sum)
+            search(x+1, limit)
+      
+      if n < 2:
+            return n
+
+      search(2, n)
+      return fibonacci[-1]
+
+
+for x in range(0, 11):
+      print('24- Fibonacci: ', x, ' - ', nth_fibonacci(x))
